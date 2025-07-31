fc8a805a7308dfdadaafef8101cb4ccf763a197c
Gabriel Sokol <gsokol4@gmail.com>
2025-07-31 14:00:56 -0500

    add tag manager

--- components/GoogleAnalytics/GoogleAnalytics.js
+++ components/GoogleAnalytics/GoogleAnalytics.js
@@ -1,11 +1,10 @@
-
-// components/GoogleAnalytics/GoogleAnalytics.js
 'use client';
 
 import Script from 'next/script';
 
 export default function GoogleAnalytics() {
   const GA_MEASUREMENT_ID = 'G-4N5C3C25ZJ';
+  const GTM_ID = 'GTM-NWWZR3ND';
 
   return (
     <>
@@ -22,6 +21,27 @@ export default function GoogleAnalytics() {
           gtag('config', '${GA_MEASUREMENT_ID}');
         `}
       </Script>
+
+      {/* Google Tag Manager */}
+      <Script id="gtm-init" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+
+      {/* Google Tag Manager (noscript) fallback */}
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
     </>
   );
 }

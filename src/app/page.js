a74ed177164c1d9dda0891ea15f5bb4906b6cc28
Gabriel Sokol <gsokol4@gmail.com>
2025-08-10 16:52:46 -0500

    adds the contact form to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 // app/page.jsx
 import HeroSection from '../../components/HeroSection/HeroSection';
 import GardenServices from '../../components/GardenServices/GardenServices';
+import ContactForm from '../../components/ContactForm/ContactForm';
+
+import styles from './page.module.css';
 
 export const metadata = {
   title: "Rose Pointe Garden Company | Get A Garden Today",
@@ -10,13 +13,13 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <HeroSection backgroundImage="/homepage-hero.webp" />
       <GardenServices 
           title="Our Premium Services" 
           description="Discover how we can transform your outdoor space with our expert services."
         />
-      {/* Your other page content */}
+      <ContactForm />
     </main>
   );
 }
